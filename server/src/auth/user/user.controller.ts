cedd974de5dcdc65ad6aeb2bdb8a29a2c7d99361
ThamerAyachi <thamerjak3@gmail.com>
2022-12-26 14:42:55 +0100

    add fetch user by id ans email

--- server/src/auth/user/user.controller.ts
+++ server/src/auth/user/user.controller.ts
@@ -1,7 +1,9 @@
 import {
+	BadRequestException,
 	ClassSerializerInterceptor,
 	Controller,
 	Get,
+	Param,
 	UseInterceptors,
 } from '@nestjs/common';
 import { AuthService } from '../auth.service';
@@ -15,5 +17,26 @@ export class UserController {
 	@UseInterceptors(ClassSerializerInterceptor)
 	async fetchAllUsers(): Promise<SerializedUser[]> {
 		return await this.userService.getAllUsers();
+	}
+
+	@Get('email/:email')
+	@UseInterceptors(ClassSerializerInterceptor)
+	async fetchUserByEmail(
+		@Param('email') email: string,
+	): Promise<SerializedUser> {
+		const user = await this.userService.findOneBy({ email });
+		if (!user)
+			throw new BadRequestException(`User with email: "${email}" not found`);
+
+		return new SerializedUser(user);
+	}
+
+	@Get('id/:id')
+	@UseInterceptors(ClassSerializerInterceptor)
+	async fetchUserById(@Param('id') id: string): Promise<SerializedUser> {
+		const user = await this.userService.findOneBy({ id });
+		if (!user) throw new BadRequestException(`User with id: "${id}" not found`);
+
+		return new SerializedUser(user);
 	}
 }
