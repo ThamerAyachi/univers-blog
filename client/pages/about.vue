0d7bd650fb5fd12d7e581a70d769c57b4f8bd175
ThamerAyachi <thamerjak3@gmail.com>
2023-01-02 01:58:56 +0100

    add client as nuxt framework

---
+++ client/pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+	<nuxt-link to="/">Back</nuxt-link>
+		<p>hello in about page</p>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	setup () {
+		
+
+		return {}
+	}
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+	<nuxt-link to="/about">about</nuxt-link>
+		<p>hello in index page</p>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	setup () {
+		
+
+		return {}
+	}
+})
+</script>
+
+<style scoped>
+
+</style>
