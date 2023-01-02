320d0f1708eb025ad5a16c7928115e9bf52472a6
ThamerAyachi <thamerjak3@gmail.com>
2023-01-03 00:50:45 +0100

    set settings for app

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	modules: ["@nuxtjs/tailwindcss", "nuxt-icons"],
+	app: {
+		head: {
+			title: "Universe Blog",
+			meta: [
+				{
+					name: "description",
+					content:
+						"Universe Blog🧾💫 is a community of amazing peoples. We're a place where all share, stay up-to-date and grow their careers.",
+				},
+			],
+			link: [
+				{
+					rel: "icon",
+					type: "image/png",
+					href: "galaxy.png",
+				},
+			],
+		},
+	},
 });
